c2c082adf4702016adc003fc82364b0546b401c1
shahjahansamon <samoon.shahjahan@gmail.com>
2021-03-02 10:58:51 +0500

    Create SaylaniDonation.sol

---
+++ SaylaniDonation.sol
@@ -0,0 +1,73 @@
+pragma solidity ^0.5.16;
+
+contract SaylaniDonation {
+    address payable private owner;
+    enum gender {male, female}
+  
+    mapping(address => Doner) public donerList;
+    uint private collectedDonation;
+    uint private totalDoners;
+    
+    constructor () public{
+         owner = msg.sender;
+    }
+    
+    struct Doner{
+        string name;
+        address donnerAddress;
+        gender _gender;
+       
+    }
+    
+   
+    
+    function getAddress() public view returns(address){
+        return owner;
+    }
+    
+    function getBalance() public view onlyMe returns (uint){
+        return owner.balance;
+    }
+    
+    function donate(string memory name,  gender _gender) public payable {
+        require(msg.value == 6 ether, "6 ether required");
+       
+        Doner memory doner = Doner(name,msg.sender,  _gender);
+        donerList[msg.sender] = doner;
+
+        owner.transfer(msg.value);
+        collectedDonation = collectedDonation + msg.value;
+        totalDoners++;
+    }
+    
+    function getCollectedDonation() public view onlyMe returns(uint){
+        return collectedDonation;
+    }
+    
+    function getTotalDonners() public view onlyMe returns(uint){
+      
+        return totalDoners;
+    }
+    
+     function getDonnerDetails(address id) public view onlyMe returns (address,string memory){
+        
+        return(donerList[id].donnerAddress,donerList[id].name);
+    }
+    
+  
+    function close() public onlyMe{ 
+        selfdestruct(owner); 
+    }
+    
+     modifier onlyMe(){
+        if(msg.sender != owner){
+          
+        }else{
+             _;
+        }
+        
+        
+    }
+    
+
+}
